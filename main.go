7bcc56b8c826d1d8c226b2208079312b45d19b99
Amir Hossein <amirsorouri26@gmail.com>
2022-05-31 11:54:01 +0430

    main added and tested.

---
+++ main.go
@@ -0,0 +1,15 @@
+// https://gowebexamples.com/http-server/
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":6060", nil)
+}
